0dc5f5c4690364809a774db3d4ac9ddac6da6d23
nikhil-mourya-mahasos <nikhil@mahasos.com>
2024-07-29 12:56:14 +0530

    material icon added

--- src/components/DynamicInput.vue
+++ src/components/DynamicInput.vue
@@ -3,92 +3,41 @@
     class="px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
   /> -->
   <div class="text-left">
-    <label
-      v-if="isLabel"
-      :for="label"
-      class="block font-semibold text-sm text-gray-900"
-      >{{ label }}</label
-    >
+    <label v-if="isLabel" :for="label" class="block font-bold text-xs text-gray-900">{{ label }}</label>
     <div class="mt-2">
       <div class="relative">
-        <div
-          class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2"
-        >
+        <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2">
           <div v-if="isPrefixIcon" class="w-10 center">
-            <svg
-              width="20"
-              height="20"
-              viewBox="0 0 26 20"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M25.5 2.5C25.5 1.125 24.375 0 23 0H3C1.625 0 0.5 1.125 0.5 2.5V17.5C0.5 18.875 1.625 20 3 20H23C24.375 20 25.5 18.875 25.5 17.5V2.5ZM23 2.5L13 8.75L3 2.5H23ZM23 17.5H3V5L13 11.25L23 5V17.5Z"
-                fill="#232323"
-                fill-opacity="0.3"
-              />
-            </svg>
+            <span class="material-icons-outlined text-gray-400">
+             {{ icon }}
+            </span>
           </div>
-          <input
-            v-if="type !== 'textarea'"
-            :type="type"
-            :name="label"
-            @change="checkValidation"
-            @focus="checkValidation"
-            :id="label"
-            v-bind="$attrs"
-            :value="modelValue"
-            :placeholder="placeholder"
+          <input v-if="type !== 'textarea'" :type="type" :name="label" @change="checkValidation"
+            @focus="checkValidation" :id="label" v-bind="$attrs" :value="modelValue" :placeholder="placeholder"
             @input="(e) => $emit('update:modelValue', e.target.value)"
-            class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-          />
-          <textarea
-            v-else
-            :type="type"
-            :name="label"
-            @change="checkValidation"
-            @focus="checkValidation"
-            :id="label"
-            v-bind="$attrs"
-            :value="modelValue"
-            :rows="textAreaRows"
-            :placeholder="placeholder"
+            class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" />
+          <textarea v-else :type="type" :name="label" @change="checkValidation" @focus="checkValidation" :id="label"
+            v-bind="$attrs" :value="modelValue" :rows="textAreaRows" :placeholder="placeholder"
             @input="(e) => $emit('update:modelValue', e.target.value)"
-            class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-          />
+            class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" />
 
-          <div
-            @mouseover="showTooltip"
-            @mouseleave="hideTooltip"
-            v-if="isTooltip"
-            :class="name + 'btn'"
-            class="w-10 cursor-pointer center"
-          >
-            <svg
-              width="25"
-              height="25"
-              viewBox="0 0 25 25"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
+          <div @mouseover="showTooltip" @mouseleave="hideTooltip" v-if="isTooltip" :class="name + 'btn'"
+            class="w-10 cursor-pointer center">
+            <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
               <circle cx="12.5" cy="12.5" r="12" stroke="#232323" />
               <path
                 d="M13.3027 14.9961H11.4863C11.4928 14.3711 11.5482 13.86 11.6523 13.4629C11.763 13.0592 11.9421 12.6914 12.1895 12.3594C12.4368 12.0273 12.7656 11.6497 13.1758 11.2266C13.4753 10.9206 13.7487 10.6341 13.9961 10.3672C14.25 10.0938 14.4551 9.80078 14.6113 9.48828C14.7676 9.16927 14.8457 8.78841 14.8457 8.3457C14.8457 7.89648 14.7643 7.50911 14.6016 7.18359C14.4453 6.85807 14.2109 6.60742 13.8984 6.43164C13.5924 6.25586 13.2116 6.16797 12.7559 6.16797C12.3783 6.16797 12.0202 6.23633 11.6816 6.37305C11.3431 6.50977 11.0697 6.72135 10.8613 7.00781C10.653 7.28776 10.5456 7.6556 10.5391 8.11133H8.73242C8.74544 7.37565 8.92773 6.74414 9.2793 6.2168C9.63737 5.68945 10.1191 5.28581 10.7246 5.00586C11.3301 4.72591 12.0072 4.58594 12.7559 4.58594C13.5827 4.58594 14.2858 4.73568 14.8652 5.03516C15.4512 5.33464 15.8971 5.76432 16.2031 6.32422C16.5091 6.8776 16.6621 7.53516 16.6621 8.29688C16.6621 8.88281 16.5417 9.42318 16.3008 9.91797C16.0664 10.4062 15.7637 10.8652 15.3926 11.2949C15.0215 11.7246 14.6276 12.1348 14.2109 12.5254C13.8529 12.8574 13.612 13.2318 13.4883 13.6484C13.3646 14.0651 13.3027 14.5143 13.3027 14.9961ZM11.4082 18.0918C11.4082 17.7988 11.4993 17.5514 11.6816 17.3496C11.8639 17.1478 12.1276 17.0469 12.4727 17.0469C12.8242 17.0469 13.0911 17.1478 13.2734 17.3496C13.4557 17.5514 13.5469 17.7988 13.5469 18.0918C13.5469 18.3717 13.4557 18.6126 13.2734 18.8145C13.0911 19.0163 12.8242 19.1172 12.4727 19.1172C12.1276 19.1172 11.8639 19.0163 11.6816 18.8145C11.4993 18.6126 11.4082 18.3717 11.4082 18.0918Z"
-                fill="#232323"
-              />
+                fill="#232323" />
             </svg>
           </div>
-          <div
-            v-if="isTooltip"
-            :id="name + 'tooltip'"
-            class="absolute hidden right-0 bottom-10 md:bottom-10 z-10 p-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg shadow-sm"
-          >
+          <div v-if="isTooltip" :id="name + 'tooltip'"
+            class="absolute hidden right-0 bottom-10 md:bottom-10 z-10 p-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg shadow-sm">
             {{ tooltipContent }}
             <div class="tooltip-arrow"></div>
           </div>
         </div>
-        <div v-if="isValid == false" class="text-red-500">
-          {{ name }}<span> is required.</span>
+        <div v-if="isValid == false" class="text-red-500 text-sm">
+          {{ requiredMessage }}<span> is required.</span>
         </div>
       </div>
     </div>
@@ -108,9 +57,11 @@ const props = defineProps({
   isTooltip: { type: [Boolean], default: false },
   tooltipContent: { type: [String], default: "" },
   isRequired: { type: [Boolean], default: false },
+  requiredMessage: { type: [String], default: "" },
   type: { default: "text" },
   textAreaRows: { default: 4 },
   isPrefixIcon: { default: true },
+  icon: { default: "" }
 });
 const isValid = ref(true);
 const internalValue = ref(props.modelValue);
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import "./index.css";
+import 'material-icons/iconfont/material-icons.css';
 import "@/styles/index.scss";
 
 // createApp(App).use(store).use(router).mount("#app");
